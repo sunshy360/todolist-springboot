92d0b450490c213f9b8921b124c35868860a172f
Sen Dang <sdang@thoughtworks.com>
2017-09-29 00:27:35 +0800

    create a springboot restful project and implement the get method

---
+++ src/main/java/hello/TodoItem.java
@@ -0,0 +1,54 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class TodoItem {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String itemName;
+
+    private boolean done;
+
+    private String timestamp;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+
+}
---
+++ src/main/java/hello/TodoItemsRepository.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import hello.TodoItem;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface TodoItemsRepository extends CrudRepository<TodoItem, Long> {
+
+}
